ac7b80d01efa03fcd3027e8bf4ca2979284e7733
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-23 22:10:39 -0300

    Configuração Inicial do projeto ConectaPlantão  - Criação da API com Java 17 e SpringBoot  - Criação do Front com Next.js e Tailwind  - Criação do BD com Postgress e Docker

---
+++ api/src/main/java/br/com/conectaplantao/api/ConectaPlantaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.conectaplantao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaPlantaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaPlantaoApiApplication.class, args);
+	}
+
+}
